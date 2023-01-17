9fe5cfb04aaf318fb6c6a88c63468b44f0639e20
Wovasya <v37444170895@gmail.com>
2023-01-17 19:57:53 +0400

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int bonus = price / 20;
+        return bonus;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+    }
+}
